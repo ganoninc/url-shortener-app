237f482bcf9bb25396f3ee4cc9beec80cc533d87
Romain Giovanetti <ganoninc@gmail.com>
2026-01-13 18:34:49 +0100

    fix(*-services): fix local dev kafka config

--- backend/redirector-service/src/main/java/com/ganoninc/viteurlshortener/redirectorservice/config/KafkaConfig.java
+++ backend/redirector-service/src/main/java/com/ganoninc/viteurlshortener/redirectorservice/config/KafkaConfig.java
@@ -2,17 +2,25 @@ package com.ganoninc.viteurlshortener.redirectorservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${kafka.topic.creation.partition-count}")
+    private int topicCreationPartitionCount;
+
+    @Value("${kafka.topic.creation.replica-count}")
+    private int topicCreationReplicaCount;
+
     @Bean
     public NewTopic urlClickedTopic() {
         return TopicBuilder
                 .name("url-clicked")
-                .partitions(3)
-                .replicas(3)
+                .partitions(topicCreationPartitionCount)
+                .replicas(topicCreationReplicaCount)
                 .build();
     }
 }
--- backend/url-service/src/main/java/com/ganoninc/viteurlshortener/urlservice/config/KafkaConfig.java
+++ backend/url-service/src/main/java/com/ganoninc/viteurlshortener/urlservice/config/KafkaConfig.java
@@ -1,18 +1,26 @@
 package com.ganoninc.viteurlshortener.urlservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${kafka.topic.creation.partition-count}")
+    private int topicCreationPartitionCount;
+
+    @Value("${kafka.topic.creation.replica-count}")
+    private int topicCreationReplicaCount;
+
     @Bean
     public NewTopic urlCreatedTopic() {
         return TopicBuilder
                 .name("url-created")
-                .partitions(3)
-                .replicas(3)
+                .partitions(topicCreationPartitionCount)
+                .replicas(topicCreationReplicaCount)
                 .build();
     }
 }
